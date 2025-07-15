b024a2cc06ec6240ddb834f2cd0a0ad7c6c59990
Shikha Pandey <shikhapandey1291@gmail.com>
2025-07-15 16:29:01 +0530

    need testing

--- app/Http/Controllers/AttributeController.php
+++ app/Http/Controllers/AttributeController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Attribute;
-use App\Models\AttributeValue;
+///use App\Models\AttributeValue;
 
 class AttributeController extends Controller
 {
